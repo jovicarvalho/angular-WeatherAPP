71f43435738e23f0b98bd57cb77b4593ba5a17d7
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-09-26 08:18:56 -0300

    ✨feat: creating actual day component in home page

---
+++ src/app/shared/components/meteorological/actual-day/actual-day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actual-day',
+  templateUrl: './actual-day.component.html',
+  styleUrls: ['./actual-day.component.css']
+})
+export class ActualDayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
