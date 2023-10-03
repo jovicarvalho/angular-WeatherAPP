ec27696e78129db859adafb21cde1ff2a214dd90
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-10-03 09:53:05 -0300

    ✨feat: creating search-bar component

--- src/app/shared/components/search-bar/search-bar.component.spec.ts
+++ src/app/shared/components/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, tick, waitForAsync  } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchBarComponent } from './search-bar.component';
+import { ActivatedRoute } from '@angular/router';
+
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -8,7 +10,15 @@ describe('SearchBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [ SearchBarComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: 
+            "/home"
+        },
+      ],
     })
     .compileComponents();
 
@@ -20,4 +30,19 @@ describe('SearchBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should store the value of input inside "cityName" when Enter key is pressed', waitForAsync(async () => {
+    const inputElement = fixture.nativeElement.querySelector('input');
+    const cityName = 'New York';
+
+    inputElement.value = cityName;
+    inputElement.dispatchEvent(new Event('input'));
+
+    const enterEvent = new KeyboardEvent('keyup', { key: 'Enter' });
+    inputElement.dispatchEvent(enterEvent);
+
+    await fixture.whenStable(); 
+    fixture.detectChanges(); 
+    expect(component.cityName).toEqual(cityName);
+  }));
 });
--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit, ɵisListLikeIterable } from '@angular/core';
+import { Component, Input, OnInit, ɵisListLikeIterable, Output, EventEmitter , HostListener } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { MeteorologicalData } from 'src/app/core/interfaces/IMeteorologicalData';
-import { MeteorologicalDataServiceService } from 'src/app/core/services/meteorological-data-service.service';
+import { MeteorologicalDataService } from 'src/app/core/services/meteorological-data.service';
+import { Subject, timer } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,23 +15,43 @@ export class SearchBarComponent implements OnInit {
 
 
   constructor(
-    private http: HttpClient,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
+    private service: MeteorologicalDataService,
+  ) {};
   
-
-  listaMeteorologicaldata: MeteorologicalData[] = [];
-  cityName:string = "";
-
-
-
+  
   ngOnInit(): void {
   }
 
+  @Output() listaMeteorologicaldata: MeteorologicalData[] = [];
+  
+  @Output() actualDay = new EventEmitter<MeteorologicalData>();
+  cityName:string = "";
+  errorWasThrown:boolean = false;
+
+  //buscarCidade(){
+    //this.service.getByCity(this.cityName).subscribe(listaCidade=>{
+      //this.today.emit(listaCidade)
+    //})
+  //}
+
+
+  buscarDiaAtualNaCidade() {
+    if (this.cityName.length > 3)
+     this.service.getActualDayinCity(this.cityName).subscribe({
+      next:(cidade) => {
+        this.actualDay.emit(cidade);
+        this.errorWasThrown = false;
+      },
+      error:(error) => {
+        this.errorWasThrown = true;
+      }}
+    );
+  }
+
+  @Input() setIconLocationParameter:boolean = true;
+  
   setIconLocation():string {
-    const currentRoute = this.route.snapshot.routeConfig?.path;
-    currentRoute == "http://localhost:60667/home"?  "iconLocation" : "";
+    this.setIconLocationParameter ? "iconLocation" : "";
     return ""
   }
 
