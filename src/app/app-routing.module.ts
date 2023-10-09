776efb55952bd1857420e785bcf0c349d18edcd2
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-10-08 23:42:41 -0300

    ✨feat: new route List Weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateMeteorologicalDataComponent } from './pages/create-meteorological-data/create-meteorological-data.component';
+import { ListWeatherComponent } from './pages/list-weather/list-weather.component';
 
 const routes: Routes = [
   {path:'',redirectTo:"home", pathMatch:"full"},
   {path:'home', component:HomeComponent},
-  {path:'create',component:CreateMeteorologicalDataComponent}
+  {path:'create',component:CreateMeteorologicalDataComponent},
+  {path:'list',component:ListWeatherComponent}
 ];
 
 @NgModule({
