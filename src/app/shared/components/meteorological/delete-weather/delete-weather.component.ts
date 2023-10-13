cd8fa5a9989a4187d5029b05bb4bdd3577e1f44f
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-10-12 22:45:27 -0300

    ✨feat: creating delete weather component

---
+++ src/app/shared/components/meteorological/delete-weather/delete-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-weather',
+  templateUrl: './delete-weather.component.html',
+  styleUrls: ['./delete-weather.component.css']
+})
+export class DeleteWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
